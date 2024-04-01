1da6c74099e9bb5db9eef35af9d253836ef60391
udaloi <pavel.udaloi@innowise-group.com>
2024-04-01 23:10:36 +0300

    [RML-AUTH] Romel auth microservice creating. Add configuration.

---
+++ src/main/java/com/romel/auth/RomelAuthApplication.java
@@ -0,0 +1,12 @@
+package com.romel.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomelAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomelAuthApplication.class, args);
+	}
+}
